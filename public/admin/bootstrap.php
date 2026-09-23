dd48403770d8966e98882907a04156cbf351d3ce
TimLeister <tim@timleister.com>
2026-09-23 17:06:51 -0400

    Fix authentication namespace imports

--- public/admin/bootstrap.php
+++ public/admin/bootstrap.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../app/helpers.php';
 
-use MediaAuth;
-use MediaConfig;
-use MediaDatabase;
+use Media\Auth;
+use Media\Config;
+use Media\Database;
 
 Config::load(__DIR__ . '/../..');
 
