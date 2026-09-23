2a5997400bb46ea321bd86a57237a27af2f03842
TimLeister <tim@timleister.com>
2026-09-23 17:06:32 -0400

    Protect backend routes with authentication

--- public/api/upload-url.php
+++ public/api/upload-url.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../app/helpers.php';
+require_once __DIR__ . '/../admin/bootstrap.php';
 
 use Media\Config;
 use Media\Database;
