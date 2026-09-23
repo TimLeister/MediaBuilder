9c3d377a01dadd3bbc0f9666651d7db569eaa2ca
TimLeister <tim@timleister.com>
2026-09-23 17:17:13 -0400

    Add asynchronous event download tools/cleanup-downloads.php

---
+++ tools/cleanup-downloads.php
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use MediaConfig;
+use MediaDatabase;
+use MediaDownloadJob;
+
+Config::load(__DIR__ . '/..');
+
+$db = Database::connection();
+$jobs = new DownloadJob($db);
+
+foreach ($jobs->expired() as $job) {
+    $archivePath = (string) ($job['archive_path'] ?? '');
+
+    if ($archivePath !== '' && is_file($archivePath)) {
+        @unlink($archivePath);
+    }
+
+    $jobDir = dirname($archivePath);
+
+    if ($jobDir !== '.' && is_dir($jobDir)) {
+        @rmdir($jobDir);
+    }
+
+    $jobs->expire((int) $job['id']);
+}
