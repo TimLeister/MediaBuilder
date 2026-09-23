51e2310c36877edbb602337d560d053d3df49bf8
TimLeister <tim@timleister.com>
2026-09-23 17:19:18 -0400

    Fix namespaces in public/api/download-list.php

--- public/api/download-list.php
+++ public/api/download-list.php
@@ -6,10 +6,10 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../app/helpers.php';
 require_once __DIR__ . '/../admin/bootstrap.php';
 
-use MediaAuth;
-use MediaConfig;
-use MediaDatabase;
-use MediaDownloadJob;
+use Media\Auth;
+use Media\Config;
+use Media\Database;
+use Media\DownloadJob;
 
 Config::load(__DIR__ . '/../..');
 
